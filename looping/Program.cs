27bc79531da16ce73ed0ec7ab0111f8b2de39eed
Amad Kazmi <smart_kazmi@hotmail.com>
2022-11-03 22:01:46 +0100

    looping in c#

---
+++ looping/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*int num = 1;
+while (num <= 20)
+Console.WriteLine(num++);
+// while loop can reduce to write the code again and again.
+
+Console.Write("please enter your age to verify your account : ?\t");
+int age = Convert.ToInt32(Console.ReadLine());
+int year = 2022;
+while (age <= 18)
+{
+ Console.Write($"Year: {year}\tAge: {age}");
+ Console.WriteLine("\t\tYou are a child, come back later.");
+    // 1 year passes
+    year++;
+    age++;
+}
+Console.WriteLine($"\nCongratulations! you are an adult in {year}.");
+//above codes in while to help user ouput and furthermore detail regards his age etc: in looping
+int x = 5;
+while (x > 6) // becoz while loop condition is flase rightaway that why no execution if x < 3 it 
+  //will print indefinit hello becoz condirion is true
+remember after while loop there is not any semi coolens.
+{
+ Console.WriteLine("hello");
+    x++;
+}
+//do while loop no matter is what ! it will gaurantee execuation of body . fx
+int x = 5;
+do
+{
+    Console.WriteLine("hello");
+    x++;
+}
+while (x > 6);*/
+
+/*Console.WriteLine("hello");// while loop with indefinte when you press again,wrong key loop finish
+string response = Console.ReadLine();//Example 1
+while (response == "again")
+{
+    Console.WriteLine("hello");
+    response = Console.ReadLine();
+}
+Console.WriteLine("you enter the wrong word");
+//Example 2
+string response;
+do
+{
+    Console.WriteLine("hello");
+    response = Console.ReadLine();
+
+}while (response == "again");
+Console.WriteLine("write wrong key");
+// same codes with 2 examples and do while is shorter and smart.
+
+int age = 10;
+bool condition = false;
+do
+{
+    Console.WriteLine($"You are {age++} years old.");
+} while (condition);
+
+//Break statement also used in switch statement while if break
+int age = 6; //age is greater then while so code will execute ! otherwise not-
+while (age > 5)
+{
+    Console.WriteLine($"You are {age++} years old.");
+    if (age > 100)
+        break;//break can be used to exit from the loop
+}*/
+
+
